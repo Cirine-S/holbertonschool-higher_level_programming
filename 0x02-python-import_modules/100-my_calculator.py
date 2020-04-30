dcd95e071f93bf5fbf0d0bc8802222bee1641b60
Cirine92 <1410@holbertonschool.com>
2020-04-30 15:50:15 +0000

    advanced calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print ("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    op = ['+', '-', '*', '/']
+    if argv[2] not in op:
+        print ("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+
+    if argv[2] == '+':
+        res = add(a, b)
+    elif argv[2] == '-':
+        res = sub(a, b)
+    elif argv[2] == '*':
+        res = mul(a, b)
+    else:
+        res = div(a, b)
+
+    print("{} {} {} = {}".format(a, argv[2], b, res))
+    exit(0)
