fce1cf867578f468487b833a8f3a028a0fd41994
Cirine92 <1410@holbertonschool.com>
2020-04-30 21:17:07 +0000

    dir builtin

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 import hidden_4
-    if __name__ == "__main__":
-        
+if __name__ == "__main__":
+    for i in range(len(dir(hidden_4))):
+        if dir(hidden_4)[i][0] != '_':
+            print('{}'.format(dir(hidden_4)[i]))
