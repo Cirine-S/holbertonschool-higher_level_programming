d2309e10ccc801081b0956860b6c4a45e1247434
Cirine92 <1410@holbertonschool.com>
2020-05-05 22:58:47 +0000

    advanced

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,17 @@
+#include <Python.h>
+
+void print_python_list_info(PyObject *p)
+{
+	size_t size, i = 0;
+	PyObject *obj;
+
+	size = PyList_Size(p);
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", obj->allocated);
+	while (i < size)
+	{
+		obj = PyList_GET_ITEM(p, i);
+		printf("Element %d: %s\n", i, obj->tp_name);
+		i++;
+	}
+}
