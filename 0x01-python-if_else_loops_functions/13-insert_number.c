3b04f78a510ef0bc8d97e74ceb08e22f71a60127
Cirine92 <1410@holbertonschool.com>
2020-05-06 12:24:41 +0000

    insert

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -30,19 +30,17 @@ listint_t *insert_node(listint_t **head, int number)
 				current = current->next;
 			}
 			else
-			{
-				if (!prev)
-				{
-					new->next = *head;
-					*head = new;
-				}
-				else
-				{
-					new->next = current;
-					prev->next = new;
-				}
 				break;
-			}
+		}
+		if (!prev)
+		{
+			new->next = *head;
+			*head = new;
+		}
+		else
+		{
+			new->next = current;
+			prev->next = new;
 		}
 	}
 	return (new);
