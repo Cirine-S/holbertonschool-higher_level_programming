2e79072fb2d872a7ae59de276e8762f5f27a7a20
Cirine92 <1410@holbertonschool.com>
2020-05-19 23:01:08 +0000

    square define 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,22 +7,20 @@ class Square:
 
     def __init__(self, size=0):
         """init"""
-
         self.size = size
 
     def area(self):
         """area"""
-
         return self.__size ** 2
 
+    @property
     def size(self):
         """size"""
-
         return self.__size
 
+    @size.setter
     def size(self, value):
         """size check"""
-
         if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
