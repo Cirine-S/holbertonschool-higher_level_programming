754f419f183c44b011379aa622aaab1ac4e1d00f
Cirine92 <1410@holbertonschool.com>
2020-06-02 22:58:28 +0000

    class inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,4 +7,4 @@ check if an obj is an inheritance of a specified super class
 def inherits_from(obj, a_class):
     if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
-        return False
+    return False
