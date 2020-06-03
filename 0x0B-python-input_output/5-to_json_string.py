4f621b07a9aa6cc08c410346db4ff1305e668baf
Cirine92 <1410@holbertonschool.com>
2020-06-03 22:49:03 +0000

    to json

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''function that returns the JSON representation of an object'''
+import json
+
+
+def to_json_string(my_obj):
+    '''function that returns the JSON representation of an object'''
+    return json.dumps(my_obj)
