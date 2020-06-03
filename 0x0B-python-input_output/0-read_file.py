41500d48dfeae9e9b4f4ede06fa47ff93d63d5f3
Cirine92 <1410@holbertonschool.com>
2020-06-03 21:09:31 +0000

    add forgotten comments

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" function to read file """
+
+
 def read_file(filename=""):
+    """ function to read file """
     with open(filename) as f:
         reaad = f.read()
     f.closed
--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" function to read lines """
+
+
 def number_of_lines(filename=""):
+    """ function to read lines """
     with open(filename) as f:
         count = len(f.readlines())
     f.closed
