64bdcf99f67b7787e004073c01eb6468c3d884b4
Cirine92 <1410@holbertonschool.com>
2020-06-05 22:27:49 +0000

    replacing the attr of Student instance

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''class module'''
+
+
+class Student:
+    '''class student'''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is not list:
+            return self.__dict__
+
+        dict = {}
+        for item in attrs:
+            if type(item) is not str:
+                return self.__dict__
+            if item in self.__dict__:
+                dict[item] = self.__dict__[item]
+        return dict
+
+    def reload_from_json(self, json):
+        for attr in json.keys():
+            self.__dict__[attr] = json[attr]
