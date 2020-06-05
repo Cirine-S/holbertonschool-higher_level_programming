9c10d290da51fc596c20667918008590258f1c00
Cirine92 <1410@holbertonschool.com>
2020-06-05 17:04:50 +0000

    load, add then save

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+import json
+from sys import argv
+
+f = 'add_item.json'
+
+try:
+    my_list = load_from_json_file(filename)
+except:
+    my_list = []
+
+my_list += argv[1:]
+
+save_to_json_file(my_list, f)
