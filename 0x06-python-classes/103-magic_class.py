34fc5ed8832c119155ff4761c0bbae21096bdd76
Cirine92 <1410@holbertonschool.com>
2020-06-06 17:04:29 +0000

    magic class translated from pycode

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -6,7 +6,7 @@ from math import pi
 class MagicClass:
     '''magic class'''
     def __init__(self, radius=0):
-        if type(radius) is not int or type(radius) is not float:
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
