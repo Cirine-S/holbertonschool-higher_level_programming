2608b3bfb88ca4d14e1a190340eba44ee180b4fb
Cirine92 <1410@holbertonschool.com>
2020-06-06 21:03:07 +0000

    rectangle subclass

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
-'''exception raise module'''
-
-
-class BaseGeometry:
-    """
-    Exception raise
-    """
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+'''subclass Rectangle module'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
