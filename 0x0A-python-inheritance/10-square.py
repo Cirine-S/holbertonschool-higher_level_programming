d9bd4183c8ba713ef53b9448c43e0d02660c7e46
Cirine92 <1410@holbertonschool.com>
2020-06-06 22:01:44 +0000

    add constructor comment

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
 '''square inherited module'''
+Rectangle = __import__('9-rectangle').Rectangle
+
 
 
 class Square(Rectangle):
     '''square inherited from Rectangle'''
     def __init__(self, size):
-        '''constructor'''
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
