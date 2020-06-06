1ca59adb6f222b6eeff7ff4d7754c95de7fb1f45
Cirine92 <1410@holbertonschool.com>
2020-06-06 00:21:31 +0000

    pascal triangle

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -12,10 +12,10 @@ def pascal_triangle(n):
     for i in range(n):
         row = []
         for j in range(i + 1):
-            if i == 0 or j == 0 or i == j:
+            if j == 0 or j == i:
                 row.append(1)
             else:
-                row.append(last[j] + last[j - 1])
-        last = row
+                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         triangle.append(row)
+
     return triangle
