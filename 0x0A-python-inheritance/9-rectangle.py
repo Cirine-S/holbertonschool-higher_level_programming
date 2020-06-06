249d318bc86ba7139004bd15ddba725fac21aa62
Cirine92 <1410@holbertonschool.com>
2020-06-06 21:38:12 +0000

    fix

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -9,13 +9,13 @@ class Rectangle(BaseGeometry):
         '''constructor'''
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     def area(self):
         '''calculates area'''
-        return self.width * self.height
+        return self.__width * self.__height
 
     def __str__(self):
         '''returns the str representation of an object'''
-        return "[Rectangle] " + str(self.width) + "/" + str(self.height)
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
