3bff74e31512c352706c431ad3644beb28623481
Cirine92 <1410@holbertonschool.com>
2020-06-06 21:59:05 +0000

    super

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+'''square inherited module'''
+
+
+class Square(Rectangle):
+    '''square inherited from Rectangle'''
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
