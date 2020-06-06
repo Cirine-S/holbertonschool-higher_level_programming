040338ade2b73bbd3a3c680d59360d73c247729d
Cirine92 <1410@holbertonschool.com>
2020-06-06 22:11:17 +0000

    str magic method for square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''square inherited module'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''square inherited from Rectangle'''
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        return '[Square] ' + str(self.__size) + '/' + str(self.__size)
