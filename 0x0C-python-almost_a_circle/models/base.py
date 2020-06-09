151d7243a25d8c0dacb1ec05d1818dea6fb433df
Cirine92 <1410@holbertonschool.com>
2020-06-09 21:37:25 +0000

    struggling trying to green checker

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 '''base file'''
-import json
 
 
 class Base:
@@ -9,7 +8,7 @@ class Base:
 
     def __init__(self, id=None):
         '''init constructor'''
-        if id:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
