ff4cd0c0e124be7612b638d5a3bc3bf71b358b10
Cirine92 <1410@holbertonschool.com>
2020-06-09 16:57:27 +0000

    almost a circle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python3
+'''inherited Rectangle'''
+from base import Base
+
+
+class Rectangle(Base):
+    '''inherited class'''
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super(Rectangle, self).__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+# properties
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+        elif width <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if type(height) is not int:
+            raise TypeError('height must be an integer')
+        elif height <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        if type(x) is not int:
+            raise TypeError('x must be an integer')
+        elif x < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        if type(y) is not int:
+            raise TypeError('y must be an integer')
+        elif y < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = y
+
+    def area(self):
+        return self.__width * self.__height
+
+    def display(self):
+        for y in range(self.__y):
+            print()
+        for i in range(self.__height):
+            for x in range(self.__x):
+                print(" ", end="")
+            for j in range(self.__width):
+                print("#", end='')
+            print()
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - \
+{}/{}".format(self.id, self.__x, self.__y, self.width, self.height)
+
+    def update(self, *args, **kwargs):
+        if len(args) > 0:
+            self.id = args[0]
+        elif "id" in kwargs:
+            self.id = kwargs["id"]
+
+        if len(args) > 1:
+            self.width = args[1]
+        elif "width" in kwargs:
+            self.width = kwargs["width"]
+
+        if len(args) > 2:
+            self.height = args[2]
+        elif "height" in kwargs:
+            self.height = kwargs["height"]
+
+        if len(args) > 3:
+            self.x = args[3]
+        elif "x" in kwargs:
+            self.x = kwargs["x"]
+
+        if len(args) > 4:
+            self.y = args[4]
+        elif "y" in kwargs:
+            self.y = kwargs["y"]
