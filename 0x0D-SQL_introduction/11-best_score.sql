4492b6da63219259e7869b1d574c339f7af344b1
cirine <1410@holbertonschool.com>
2020-07-15 00:32:34 +0100

    remove quotes

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,6 +1,6 @@
 -- lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0
 
-SELECT 'score', 'name'
+SELECT score, name
 FROM second_table
 WHERE score >= 10
-ORDER BY 'score' DESC;
+ORDER BY score DESC;
