2e19127209c972c575327333101a8298e7157967
cirine <1410@holbertonschool.com>
2020-07-15 00:29:21 +0100

    correct 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
 -- creates a table second_table, implements it with data
 
 CREATE TABLE IF NOT EXISTS second_table (id int, name VARCHAR(256), score INT);
-INSERT INTO second_table VALUES
-(1, “John”, 10),
-(2, “Alex”, 3),
-(3, “Bob”, 14),
-(4, “George”, 8);
+INSERT INTO second_table
+VALUES (1, "John", 10),
+       (2, "Alex", 3),
+       (3, "Bob", 14),
+       (4, "George", 8);
