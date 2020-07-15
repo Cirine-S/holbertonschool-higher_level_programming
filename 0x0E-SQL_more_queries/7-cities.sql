3ad4b0b5b7c43d31c5f38e06939887e48a3661a2
cirine <1410@holbertonschool.com>
2020-07-15 22:49:34 +0100

    trying to green red output check

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,7 @@
 -- creates the database hbtn_0d_usa and the table states
-
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     FOREIGN KEY (state_id) REFERENCES states(id));
