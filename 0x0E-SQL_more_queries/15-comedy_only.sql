c9d58fd111fc71154fa7d98cf9a300ea70112db0
cirine <1410@holbertonschool.com>
2020-07-15 23:16:10 +0100

    correct JOIN

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -2,7 +2,7 @@
 
 SELECT tv_shows.title
 FROM tv_shows
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title;
