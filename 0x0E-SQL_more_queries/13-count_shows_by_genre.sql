b3af49618e4f85c50ce982bfd21c986e5681f2c6
cirine <1410@holbertonschool.com>
2020-07-15 23:12:42 +0100

    let's join

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,4 +4,4 @@ SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_show_genres
 INNER JOIN tv_genres 
 ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY number_of_shows DESC;
+GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
