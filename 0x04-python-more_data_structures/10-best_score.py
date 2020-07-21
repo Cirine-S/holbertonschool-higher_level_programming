7eca946ea071e84031f144f358857ded6808249d
cirine <1410@holbertonschool.com>
2020-07-22 00:50:55 +0100

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,4 +3,10 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     else:
-        return (max(a_dictionary))
+        max = 0
+        max_key = ''
+        for key, value in a_dictionary.items():
+            if max < value:
+                max = value
+                max_key = key
+        return (max_key)
