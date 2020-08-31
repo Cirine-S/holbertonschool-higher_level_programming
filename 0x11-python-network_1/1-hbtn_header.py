520437b85e6e237325bb48787bc2a3f332438d76
cirine <1410@holbertonschool.com>
2020-08-31 22:20:38 +0100

    changing doc's place

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+''' getheader of X-Request-Id '''
 from urllib import request
 import sys
-''' getheader of X-Request-Id '''
 
 
-if __name__ == "__main__":
 with request.urlopen(sys.argv[1]) as response:
     http_header = response.getheader('X-Request-Id')
 print(http_header)
