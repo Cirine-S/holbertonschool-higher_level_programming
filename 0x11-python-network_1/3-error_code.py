7079ca5c64535a76a8fe2af863695b43b54ea0bb
cirine <1410@holbertonschool.com>
2020-09-01 00:01:06 +0100

    get html page & handle errors

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" get request with error code handling"""
+from urllib import request
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(sys.argv[1]) as html:
+            html = response.read().decode()
+            print(html)
+    except HTTPError as r:
+        print('Error code: {}'.format(r.code))
