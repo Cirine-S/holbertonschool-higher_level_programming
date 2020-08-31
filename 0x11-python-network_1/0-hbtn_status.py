e439cf0339fd96f20d8596b1d83ca887ac1a5549
cirine <1410@holbertonschool.com>
2020-08-31 21:05:35 +0100

    body response details

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from urllib import request
+''' fetch https://intranet.hbtn.io/status '''
+
+
+with request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+print('Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}\
+'.format(html.__class__, html, html.decode('utf_8')))
