63346577de77689d1dd772bccc5274d86182b709
cirine <1410@holbertonschool.com>
2020-09-01 00:44:58 +0100

    get request with error handled

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,6 +7,6 @@ import sys
 if __name__ == "__main__":
     r = requests.get(sys.argv[1])
     if r.status_code >= 400:
-        print('Error code: {}'.format(r.code))
+        print('Error code: {}'.format(r.status_code))
     else:
         print(r.text)
