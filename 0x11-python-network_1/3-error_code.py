d10e3f8ad780fab8b79d641dd549003557a72177
cirine <1410@holbertonschool.com>
2020-09-01 00:06:25 +0100

    modifying import params

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ get request with error code handling"""
-from urllib import request
+from urllib.error import HTTPError
+from urllib.request import Request, urlopen
 import sys
 
 
@@ -8,6 +9,6 @@ if __name__ == "__main__":
     try:
         with request.urlopen(sys.argv[1]) as response:
             html = response.read().decode()
-            print(html)
-    except HTTPError as r:
-        print('Error code: {}'.format(r.code))
+        print(html)
+    except HTTPError as e:
+        print('Error code: {}'.format(e.code))
