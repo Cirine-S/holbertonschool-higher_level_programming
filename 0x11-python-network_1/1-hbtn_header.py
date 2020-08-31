8cef08268c8e92086c4df4a11d74441da16651dd
cirine <1410@holbertonschool.com>
2020-08-31 22:35:06 +0100

    fetching where checker didn't agree

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,6 +4,7 @@ from urllib import request
 import sys
 
 
-with request.urlopen(request.Request(sys.argv[1])) as response:
-    http_header = response.getheader('X-Request-Id')
-print(http_header)
+if __name__ == "__main__":
+    with request.urlopen(sys.argv[1]) as response:
+        http_header = response.getheader('X-Request-Id')
+    print(http_header)
