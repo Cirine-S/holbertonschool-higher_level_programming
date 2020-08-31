0d8eb44a4e2e9b8917a8a70230b10ddc71da8636
cirine <1410@holbertonschool.com>
2020-08-31 22:18:36 +0100

    get header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import urllib.request as request
+from urllib import request
 import sys
 ''' getheader of X-Request-Id '''
 
 
+if __name__ == "__main__":
 with request.urlopen(sys.argv[1]) as response:
     http_header = response.getheader('X-Request-Id')
 print(http_header)
