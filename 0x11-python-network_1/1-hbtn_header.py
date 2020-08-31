e53e1aee013c35fea9dbb5375e369db36d8c3384
cirine <1410@holbertonschool.com>
2020-08-31 22:24:25 +0100

    if name added

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,6 +4,7 @@ from urllib import request
 import sys
 
 
+if __name__ == "__main__":
 with request.urlopen(sys.argv[1]) as response:
     http_header = response.getheader('X-Request-Id')
 print(http_header)
