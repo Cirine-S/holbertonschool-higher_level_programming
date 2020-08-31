a0d5df35cde8f1a4181a81fe0a1b75c23eb1cfec
cirine <1410@holbertonschool.com>
2020-08-31 22:27:23 +0100

    fetching where checker didn't agree

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""task 1"""
-import urllib.request as request
+""" A script that fetches URL  """
+from urllib import request
 import sys
+
 
 if __name__ == "__main__":
     with request.urlopen(sys.argv[1]) as s:
