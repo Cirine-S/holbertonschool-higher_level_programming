0b39d2e5d2277eeb44aa3e5419cb82c893574d52
cirine <1410@holbertonschool.com>
2020-09-01 01:26:56 +0100

    github Credentials

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" takes my Github credentials & display my id """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    pwd = sys.argv[2]
+    r = requests.get('https://api.github.com/user', auth=(username, pwd))
+    print(r.json()['id'])
