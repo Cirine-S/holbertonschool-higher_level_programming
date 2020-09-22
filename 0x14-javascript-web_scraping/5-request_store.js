88ea21b3c27ead47c7cc78685f52a893d8593c86
cirine <1410@holbertonschool.com>
2020-09-23 00:14:04 +0100

    lorem ipsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,21 +2,12 @@
 const request = require('request');
 const fs = require('fs');
 
-const options = {
-  url: process.argv[2],
-  method: 'GET'
-};
-
-request(options, (err, reponse, body) => {
+request(process.argv[2], (err, body) => {
   if (err) {
-    return console.log(err);
+    console.log(err);
   }
 
   fs.writeFile(process.argv[3], body,
-    {
-      encoding: 'utf8',
-      flag: 'w'
-    },
     (err) => {
       if (err) { console.log(err); }
     });
