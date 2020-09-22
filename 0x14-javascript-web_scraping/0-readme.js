4f9e314eac94b1759bba58db4b8f133c3ae54658
cirine <1410@holbertonschool.com>
2020-09-22 23:06:46 +0100

    README file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-const fs = require('fs') 
+const fs = require('fs');
 
-fs.readFile(process.argv[2], (err, data) => { 
-    if (err) {
-        console.log(err);
-    }
-  else {
-console.log(data.toString()); 
-}
-})
+fs.readFile(process.argv[2], (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
+  }
+});
