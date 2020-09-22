3ff2460a89c7af6ef72b5203fcbddd35aa8c7e46
cirine <1410@holbertonschool.com>
2020-09-23 00:22:27 +0100

    copy from url to file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,13 +2,12 @@
 const request = require('request');
 const fs = require('fs');
 
-request(process.argv[2], (err, body) => {
+request(process.argv[2], (err, response, body) => {
   if (err) {
     console.log(err);
   }
 
-  fs.writeFile(process.argv[3], body,
-    (err) => {
-      if (err) { console.log(err); }
-    });
+  fs.writeFile(process.argv[3], body, { flag: 'w' }, err => {
+    if (err) { console.log(err); }
+  });
 });
