4fdeff422f3a63e96c79762bbfadb1902346eae4
cirine <1410@holbertonschool.com>
2020-09-23 00:52:56 +0100

    JSONtreatment

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,25 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
-const dt = {};
 
-const options = {
-  url: process.argv[2],
-  method: 'GET'
-};
-
-request(options, (err, reponse, body) => {
+request(process.argv[2], (err, reponse, body) => {
   if (err) {
     return console.log(err);
   }
-  for (const i of JSON.parse(body)) {
-    const j = i.userId;
-    dt[j] = 0;
-  }
-  for (const cn of JSON.parse(body)) {
-    if (cn.completed) {
-      dt[cn.userId] = dt[cn.userId] + 1;
+
+  const DoneDict = {};
+  const todos = JSON.parse(body);
+
+  for (const i in JSON.parse(body)) {
+    if (todos[i].completed) {
+      if (DoneDict[todos[i].userId] === undefined) {
+        DoneDict[todos[i].userId] = 0;
+      }
+      DoneDict[todos[i].userId]++;
     }
   }
-
-  console.log(dt);
+  console.log(DoneDict);
 });
