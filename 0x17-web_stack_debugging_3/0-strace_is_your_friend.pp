eb5a5608dabbda3e9346e332e68399ba6f1ca6da
cirine <1410@holbertonschool.com>
2020-09-30 18:19:47 +0100

    rm

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# Puppet manifesto
-exec { 'error fix':
-  path     => ['/bin', '/usr/bin', '/usr/sbin'],
-  provider => 'shell',
-  command  => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-}
